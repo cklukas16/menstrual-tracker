a9cd83a72aa76d286d5db54442ca1c51052ee9f5
cklukas16 <149278653+cklukas16@users.noreply.github.com>
2024-04-21 15:49:47 -0500

    Modified table column names in schema and Daily record to avoid Postgres keywords.

--- src/main/java/edu/stthomas/menstrualtracker/model/Daily.java
+++ src/main/java/edu/stthomas/menstrualtracker/model/Daily.java
@@ -3,11 +3,14 @@ package edu.stthomas.menstrualtracker.model;
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 public record Daily (
         @Id   
-        Integer id,    
+        Integer id,
+        @Column(value = "dateAdded")    // avoids Postgres reserved word
         LocalDate date,
+        @Column(value = "weightLbs")    // avoids Postgres reserved word
         double weight,
         Level mood,
         Level bloating,
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,9 @@
+-- POSTGRES version for production database: 
+
 CREATE TABLE IF NOT EXISTS Daily (
     id SERIAL PRIMARY KEY,
-    `date` DATE NOT NULL,
-    `weight` decimal(4,1),
+    dateAdded DATE NOT NULL,
+    weightLbs decimal(4,1),
     mood VARCHAR(10),
     bloating VARCHAR(10),
     cramps VARCHAR(10),
@@ -13,3 +15,21 @@ CREATE TABLE IF NOT EXISTS Daily (
     appetite VARCHAR(10),
     comments text
 );
+
+-- SQL version for H2 Database
+
+-- CREATE TABLE IF NOT EXISTS Daily (
+--     id SERIAL PRIMARY KEY,
+--     `date` DATE NOT NULL,
+--     `weight` decimal(4,1),
+--     mood VARCHAR(10),
+--     bloating VARCHAR(10),
+--     cramps VARCHAR(10),
+--     headache VARCHAR(10),
+--     irritability VARCHAR(10),
+--     energy VARCHAR(10),
+--     anxiety VARCHAR(10),
+--     breast_tenderness VARCHAR(10),
+--     appetite VARCHAR(10),
+--     comments text
+-- );
