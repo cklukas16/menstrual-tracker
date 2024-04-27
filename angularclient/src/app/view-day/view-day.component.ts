7347ae14361305041eaf8ba115cf8deacd9dcd0f
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-04-27 13:52:37 -0500

    Defaults datepicker to today (#8)

--- angularclient/src/app/view-day/view-day.component.ts
+++ angularclient/src/app/view-day/view-day.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-view-day',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './view-day.component.css'
 })
 export class ViewDayComponent {
+
+  today: NgbDateStruct;
+  selectedDay: NgbDateStruct;
+  convertedDay: String;
+
+  //defaults datePicker to today
+  constructor() {
+    const today = new Date();
+    this.today = { year: today.getFullYear(), month: today.getMonth() + 1, day: today.getDate() };
+  }
+
+  //converts date to string & sends request to backend
+  onDateSelect(selectedDay: NgbDateStruct) {
+    this.selectedDay = selectedDay;
+    this.convertedDay = `${this.selectedDay.year}-${this.selectedDay.month}-${this.selectedDay.day}`
+  }
 
 }
