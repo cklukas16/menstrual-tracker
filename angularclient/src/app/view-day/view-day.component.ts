8181f0f25e4458dafc19e527740c6ad78e9304ac
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-04-27 12:36:24 -0500

    Adds daily-view component (#8)

---
+++ angularclient/src/app/view-day/view-day.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-day',
+  templateUrl: './view-day.component.html',
+  styleUrl: './view-day.component.css'
+})
+export class ViewDayComponent {
+
+}
