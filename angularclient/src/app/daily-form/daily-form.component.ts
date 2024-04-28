ebd1a9a33f19f4f7950d16a763984fe44738bfe4
cklukas16 <149278653+cklukas16@users.noreply.github.com>
2024-04-27 19:09:53 -0500

    Add Daily page modified to remove required components and improve spacing. Client-side alert window now appears if entry already exists for date.

--- angularclient/src/app/daily-form/daily-form.component.ts
+++ angularclient/src/app/daily-form/daily-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Daily } from '../model/daily';
 import { DailyService } from '../service/daily.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-daily-form',
@@ -18,7 +19,16 @@ export class DailyFormComponent {
   }
 
   onSubmit(){
-    this.dailyService.save(this.daily).subscribe(result => this.gotoDailyList());
+    this.dailyService.save(this.daily).subscribe(result => this.gotoDailyList(), 
+      err => {
+        if (err instanceof HttpErrorResponse){
+          const errorMessage = new Array<{propName: string; errors: string}>();
+
+          if (err.status ===406){
+            window.alert("An entry for this date already exists.")
+          }
+        }
+      });
   }
 
   gotoDailyList(){
