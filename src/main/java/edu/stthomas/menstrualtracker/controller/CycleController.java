3c26c3863d44b2fc3012511c473c47c05154a6e5
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-04-28 14:27:53 -0500

    Adds CycleController (#5)

---
+++ src/main/java/edu/stthomas/menstrualtracker/controller/CycleController.java
@@ -0,0 +1,29 @@
+package edu.stthomas.menstrualtracker.controller;
+
+import edu.stthomas.menstrualtracker.model.Cycle;
+import edu.stthomas.menstrualtracker.repository.CycleRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cycle")
+@CrossOrigin
+public class CycleController {
+
+    private final CycleRepository repository;
+
+    public CycleController(CycleRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Cycle> findAll() {
+        return repository.findAll();
+    }
+
+}
