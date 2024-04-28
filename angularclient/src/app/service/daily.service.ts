f1db376b3f3ea18373edf517d4eb864b5a7b2998
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-04-27 21:34:15 -0500

    Views by date (#8)

--- angularclient/src/app/service/daily.service.ts
+++ angularclient/src/app/service/daily.service.ts
@@ -9,13 +9,19 @@ import { Observable } from 'rxjs';
 export class DailyService {
 
   private dailyUrl: string;
+  private dateUrl: string;
 
   constructor(private http: HttpClient) {
     this.dailyUrl = 'http://localhost:8080/api/daily';
+    this.dateUrl = 'http://localhost:8080/api/daily/filter/'
   }
 
   public findAll(): Observable<Daily[]> {
     return this.http.get<Daily[]>(this.dailyUrl);
+  }
+
+  public findDate(date: string): Observable<Daily[]> {
+    return this.http.get<Daily[]>(`${this.dateUrl}${date}`);
   }
 
   public save(daily: Daily) {
