726d9aef682f3178b47b9728a9826d484ee79062
cklukas16 <149278653+cklukas16@users.noreply.github.com>
2024-04-29 23:41:33 +0000

    Modified CORS to accept all

--- src/main/java/edu/stthomas/menstrualtracker/controller/DailyController.java
+++ src/main/java/edu/stthomas/menstrualtracker/controller/DailyController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/daily")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class DailyController {
 
     private final DailyRepository repository;
