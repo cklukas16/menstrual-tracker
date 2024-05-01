ef6ac7dc54c6058a6008b94fe74aba3e4d5d5dea
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-04-30 19:04:16 -0500

    Adds cycle component (#5)

---
+++ angularclient/src/app/cycle-info/cycle-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cycle-info',
+  templateUrl: './cycle-info.component.html',
+  styleUrl: './cycle-info.component.css'
+})
+export class CycleInfoComponent {
+
+}
