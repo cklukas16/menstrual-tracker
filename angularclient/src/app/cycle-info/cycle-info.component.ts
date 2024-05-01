788333c77c8c5500b62c192602965ef781e8c7f6
Elizabeth Rowley <Elizabeth.Rowley@bsci.com>
2024-04-30 20:55:07 -0500

    Calculates cycle info (#10, #11, #12)

--- angularclient/src/app/cycle-info/cycle-info.component.ts
+++ angularclient/src/app/cycle-info/cycle-info.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class CycleInfoComponent {
 
+  day: number = 5;
+  phase: string;
+  exercise: string;
+  nutrition: string;
+  fast: number;
+
+  ngOnInit(): void {
+    this.getCycleInfo(this.day);
+    this.getFasting(this.day);
+  }
+
+  getCycleInfo(day: number) {
+    if(day > 0 && day < 11) {
+      this.phase = "Power Phase 1";
+      this.exercise = "High Intensity";
+      this.nutrition = "Ketobiotic"
+    } else if(day > 10 && day < 16) {
+      this.phase = "Manifestation"
+      this.exercise = "Strength";
+      this.nutrition = "Hormone Feasting"
+    } else if(day > 15 && day < 20) {
+      this.phase = "Power Phase 2"
+      this.exercise = "High Intensity";
+      this.nutrition = "Ketobiotic"
+    } else if(day > 20) {
+      this.phase = "Nurture"
+      this.exercise = "Light";
+      this.nutrition = "Hormone Feasting"
+    } else {
+      this.phase = ""
+      this.exercise = "";
+      this.nutrition = ""
+    }
+  }
+
+  getFasting(day: number) {
+    if((day > 0 && day <5) || (day > 10  && day < 16)){
+      this.fast = 13;
+    } else if(day == 5 || (day > 15 && day < 20)) {
+      this.fast = 17;
+    } else if(day > 5 && day < 11) {
+      this.fast = 17;
+    } else {
+      this.fast = 0;
+    }
+  }
 }
